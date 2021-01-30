5f5151236b7980754f7c782a2d9f1fa59cdbb59e
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-30 11:26:11 -0700

    models using the useState hook

--- kaelin-blog/src/Home.js
+++ kaelin-blog/src/Home.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
 
+    const [name, setName] = useState('Kaelin');
+    const [age, setAge] = useState(35)
+
     const handleClick = (name, e) => {
+        setName('Kaelin Alexander')
+        setAge(age + 1)
         console.log('Hello, ' + name + '!')
-        console.log(e)
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old!</p>
             <button onClick={(e) => handleClick('Kaelin Alexander', e)}>Click Me</button>
         </div>
      );
