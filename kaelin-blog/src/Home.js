4918131250bee5091e94a2a221ccb6b34aab0c54
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-30 11:37:00 -0700

    strips back homepage for refactoring from fresh

--- kaelin-blog/src/Home.js
+++ kaelin-blog/src/Home.js
@@ -2,20 +2,9 @@ import { useState } from 'react';
 
 const Home = () => {
 
-    const [name, setName] = useState('Kaelin');
-    const [age, setAge] = useState(35)
-
-    const handleClick = (name, e) => {
-        setName('Kaelin Alexander')
-        setAge(age + 1)
-        console.log('Hello, ' + name + '!')
-    }
-
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name } is { age } years old!</p>
-            <button onClick={(e) => handleClick('Kaelin Alexander', e)}>Click Me</button>
         </div>
      );
 }
