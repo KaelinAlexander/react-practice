dc72cc9ab1fab1bf8e9b4d447d6062392fb950be
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-30 11:53:25 -0700

    models mapping through an array to generate template

--- kaelin-blog/src/Home.js
+++ kaelin-blog/src/Home.js
@@ -2,9 +2,20 @@ import { useState } from 'react';
 
 const Home = () => {
 
+    const [blogs, setBlogs] = useState([
+        {title: 'Sample Blog 1', body: 'Pokem ipsum dolor sit amet Typhlosion Rare Candy Mew Durant Crustle Foongus. Fire Hoothoot Razor Leaf Pewter City Murkrow Drapion Vine Whip.', author: 'Kaelin', id: 1},
+        {title: 'Sample Blog 2', body: 'Pokem ipsum dolor sit amet Typhlosion Rare Candy Mew Durant Crustle Foongus. Fire Hoothoot Razor Leaf Pewter City Murkrow Drapion Vine Whip.', author: 'Kaelin', id: 2},
+        {title: 'Sample Blog 3', body: 'Pokem ipsum dolor sit amet Typhlosion Rare Candy Mew Durant Crustle Foongus. Fire Hoothoot Razor Leaf Pewter City Murkrow Drapion Vine Whip.', author: 'Kaelin', id: 3}
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Wirtten by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
