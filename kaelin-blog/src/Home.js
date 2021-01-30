750a302b3888e07d26e9537153cc09e9e409f06b
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-30 11:12:42 -0700

    models click events and the event object

--- kaelin-blog/src/Home.js
+++ kaelin-blog/src/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
+
+    const handleClick = (name, e) => {
+        console.log('Hello, ' + name + '!')
+        console.log(e)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={(e) => handleClick('Kaelin Alexander', e)}>Click Me</button>
         </div>
      );
 }
