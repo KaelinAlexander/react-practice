f7e6a632842fe7ec549768abfc0e709349607a44
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-31 08:29:14 -0700

    models reusing components, flexibly, with a simple props filter

--- kaelin-blog/src/Home.js
+++ kaelin-blog/src/Home.js
@@ -5,13 +5,14 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState([
         {title: 'Sample Blog 1', body: 'Pokem ipsum dolor sit amet Typhlosion Rare Candy Mew Durant Crustle Foongus. Fire Hoothoot Razor Leaf Pewter City Murkrow Drapion Vine Whip.', author: 'Kaelin', id: 1},
-        {title: 'Sample Blog 2', body: 'Pokem ipsum dolor sit amet Typhlosion Rare Candy Mew Durant Crustle Foongus. Fire Hoothoot Razor Leaf Pewter City Murkrow Drapion Vine Whip.', author: 'Kaelin', id: 2},
+        {title: 'Sample Blog 2', body: 'Pokem ipsum dolor sit amet Typhlosion Rare Candy Mew Durant Crustle Foongus. Fire Hoothoot Razor Leaf Pewter City Murkrow Drapion Vine Whip.', author: 'Justin', id: 2},
         {title: 'Sample Blog 3', body: 'Pokem ipsum dolor sit amet Typhlosion Rare Candy Mew Durant Crustle Foongus. Fire Hoothoot Razor Leaf Pewter City Murkrow Drapion Vine Whip.', author: 'Kaelin', id: 3}
     ]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === "Kaelin" )} title="Kaelin's Blogs" />
         </div>
      );
 }
