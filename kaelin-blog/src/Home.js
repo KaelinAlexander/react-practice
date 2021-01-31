af3385f7ccd97cbb7c34ce7c81c685307f27952e
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-31 08:41:28 -0700

    models passing functions as props in order to manipulate data in parent component

--- kaelin-blog/src/Home.js
+++ kaelin-blog/src/Home.js
@@ -9,10 +9,15 @@ const Home = () => {
         {title: 'Sample Blog 3', body: 'Pokem ipsum dolor sit amet Typhlosion Rare Candy Mew Durant Crustle Foongus. Fire Hoothoot Razor Leaf Pewter City Murkrow Drapion Vine Whip.', author: 'Kaelin', id: 3}
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === "Kaelin" )} title="Kaelin's Blogs" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === "Kaelin" )} title="Kaelin's Blogs" handleDelete={handleDelete} />
         </div>
      );
 }
