2f050e85581fc515a7a8fbe37e972cfa606d523d
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-01 15:01:00 -0700

    models using ui-based aspects of state to show loading message

--- kaelin-blog/src/Home.js
+++ kaelin-blog/src/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
@@ -11,13 +12,14 @@ const Home = () => {
                 return res.json()
             })
             .then((data) => {
-                console.log(data)
                 setBlogs(data)
+                setIsPending(false)
             })        
     }, []);
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
      );
