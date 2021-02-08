cac84bcc7f264e96c8840337711a24bf0f904f13
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-08 08:50:43 -0700

    adds initial form w/o controlled inputs

--- kaelin-blog/src/Create.js
+++ kaelin-blog/src/Create.js
@@ -2,6 +2,23 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input
+                type="text"
+                required
+                />
+                <label>Blog Body:</label>
+                <textarea
+                required
+                ></textarea>
+                <label>Blog Author:</label>
+                <select>
+                    <option value="Kaelin">Kaelin</option>
+                    <option value="Justin">Justin</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
