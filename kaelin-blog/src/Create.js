1f901f55836de990236091c2c7cdafed6fe0650c
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-08 09:11:45 -0700

    models using state with controlled inputs

--- kaelin-blog/src/Create.js
+++ kaelin-blog/src/Create.js
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Kaelin');
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
@@ -7,13 +14,20 @@ const Create = () => {
                 <input
                 type="text"
                 required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog Body:</label>
                 <textarea
                 required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Blog Author:</label>
-                <select>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="Kaelin">Kaelin</option>
                     <option value="Justin">Justin</option>
                 </select>
