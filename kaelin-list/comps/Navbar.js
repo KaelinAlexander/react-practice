5b422f1ea9bf97a8ae0a703c31401fed043be145
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-09 14:25:32 -0700

    adds modular components from elsewhere in file structure

---
+++ kaelin-list/comps/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className="logo">
+                <h1>Friend List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Friend Listing</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
--- kaelin-list/pages/index.js
+++ kaelin-list/pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repudiandae, eius praesentium facere doloremque iusto omnis ab magni magnam neque consequatur aut itaque asperiores debitis soluta expedita quas, obcaecati earum!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda perferendis quae enim laboriosam iure possimus pariatur numquam molestiae. Dolores magni officiis in iure dolorem. Animi quibusdam ipsa quidem! Nisi?</p>
+      <Footer />
     </div>
   )
 }
