e875a3ee2f33995d19ffd7a13c16a3180bc8fd9d
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-10 08:55:33 -0700

    adds a layout wrapper to keep things DRY

--- kaelin-list/pages/index.js
+++ kaelin-list/pages/index.js
@@ -7,12 +7,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus repudiandae, eius praesentium facere doloremque iusto omnis ab magni magnam neque consequatur aut itaque asperiores debitis soluta expedita quas, obcaecati earum!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda perferendis quae enim laboriosam iure possimus pariatur numquam molestiae. Dolores magni officiis in iure dolorem. Animi quibusdam ipsa quidem! Nisi?</p>
       <Link href="/friends"><a>See Friend Listing</a></Link>
-      <Footer />
     </div>
   )
 }
