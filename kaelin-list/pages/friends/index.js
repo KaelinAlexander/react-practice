283246da1a9efeeb1cdc3098418d4421fd13912e
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-12 10:57:31 -0700

    models using getStaticProps to issue a fetch before SSR

--- kaelin-list/pages/friends/index.js
+++ kaelin-list/pages/friends/index.js
@@ -1,7 +1,25 @@
-const Friends = () => {
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { friends: data }
+    }
+
+}
+
+const Friends = ({ friends }) => {
     return ( 
         <div>
             <h1>All Friends</h1>
+            {friends.map(friend => (
+                <div key={friend.id}>
+                    <a>
+                        <h3>{friend.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
